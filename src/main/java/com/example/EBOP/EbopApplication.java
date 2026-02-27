adfb293cb80c4a0cd92ea0d87d797a39bf07b44a
Developer <user@example.com>
2026-02-27 18:04:32 +0530

    Initial commit: EBOP Enterprise Banking Operations Portal

---
+++ src/main/java/com/example/EBOP/EbopApplication.java
@@ -0,0 +1,13 @@
+package com.example.EBOP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EBOP/EbopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EBOP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
